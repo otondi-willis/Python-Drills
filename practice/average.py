3107056822ed764f1e127cb86b5fa03d25a88fa0
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-03 07:58:46 +0300

    finding average in a loop

---
+++ practice/average.py
@@ -0,0 +1,12 @@
+
+#Summing in a Loop
+sum=0
+count=0
+print("Before Finding Average", sum)
+for thing in [23,4,7,8,34,26,78]:
+    sum += thing
+    count += 1
+
+    
+print("After summing", sum)
+print("After average", sum/count)
