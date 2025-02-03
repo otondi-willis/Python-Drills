e4b004a679e523f083b8ffe3b59ff09cdb28baed
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-03 07:53:55 +0300

    Summing in a loop

--- practice/sum.py
+++ practice/sum.py
@@ -1,8 +1,8 @@
 
-#Counting in a Loop
+#Summing in a Loop
 initial=0
 print("Before", initial)
 for thing in [23,4,7,8,34,26,78]:
-    initial += 1
-    print(initial,thing)
+    initial += thing
+    
 print("After", initial)
