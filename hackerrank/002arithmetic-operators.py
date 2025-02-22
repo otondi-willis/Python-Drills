db76be0db8e8a1f773d6b5f09a3ea845da24b463
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-22 17:33:44 +0300

    hackerrank:arithmetic operations drill

---
+++ hackerrank/002arithmetic-operators.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    
+    #input the first number
+    a = int(input())
+    
+    #input the second number
+    b = int(input())
+    
+    #sum
+    print(a + b)
+    
+    #subtraction
+    print(a - b)
+    
+    #multiplication
+    print(a * b)
