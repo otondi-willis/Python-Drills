7c32bd17c0b21e4d4f60f5833afe7aed8b4833e8
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-24 02:41:09 +0300

    hackerank: functions problem

---
+++ hackerrank/005functions.py
@@ -0,0 +1,17 @@
+#use of functions to determine leap year in a Gregorian Calendar
+def is_leap(year):
+    leap = False
+    if year%4 == 0:
+        if year%100 != 0:
+            leap = True 
+        if year%400 == 0:
+            leap = True  
+    
+    else:
+        leap = False
+    return leap
+ 
+ 
+
+year = int(input())
+print(is_leap(year))
