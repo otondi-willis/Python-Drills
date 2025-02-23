8d86445070299331ebdf0801e0f7d037895ee40e
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-24 02:40:38 +0300

    hackerank: division problem

---
+++ hackerrank/003division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    #The result of the integer division
+    print(int(a/b))
+    #The result of the float division
+    print(float(a/b))
