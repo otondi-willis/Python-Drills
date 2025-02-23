cea49e93e03bcaa100240e0ba92d1a607121df85
otondi-willis <willis.otondi.ke@gmail.com>
2025-02-24 02:40:51 +0300

    hackerank: loops problem

---
+++ hackerrank/004loops.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+if __name__ == '__main__':
+    n = int(input())
+    m=n-1
+    c=[]
+   
+    
+    while m>=0:
+       c.append(m**2)
+       m=m-1 
+    for i in reversed(c):
+        print(i)
+    
